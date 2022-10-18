bf58578230912700673989d44ef897cc5056cfe2
Alaa Abdelbaki <a.eldeen91@gmail.com>
2022-10-18 12:33:31 +0200

    add Ez-Namey file

---
+++ javascript/javascript1/week1/smart-ease io/Ez-Namey.js
@@ -0,0 +1,5 @@
+firstWord = ['WestCoast', 'Smooth', 'Network', 'servo', 'New', 'Heavenly', 'Giant', 'Brighter', 'Pioneer', 'AllPro']
+secondWords = ['Place', 'Technology', 'Innovations', 'Systems', 'Engine', 'Industries', 'Lab', 'Research', 'Power', 'Devices']
+const randomNumber = Math.floor(Math.random() * 10);
+const result = ` ${firstWord[randomNumber]} ${secondWords[randomNumber]}`
+console.log(`The startup name is: (${result} ) and contains ${result.length} characters`)
