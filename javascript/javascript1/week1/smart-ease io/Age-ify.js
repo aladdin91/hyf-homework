6e7bf1c9aacf77d7bcd4d754d37e97326c193394
Alaa Abdelbaki <a.eldeen91@gmail.com>
2022-10-18 11:56:25 +0200

    add age-ify file

---
+++ javascript/javascript1/week1/smart-ease io/Age-ify.js
@@ -0,0 +1,4 @@
+const yearOfBirth = 1991;
+const yearFuture = 2031;
+const age = yearFuture - yearOfBirth;
+console.log(`You will be ${age} years old in ${yearFuture}`)
---
+++ javascript/javascript1/week1/smart-ease io/Goodboy-Oldboy.js
@@ -0,0 +1,3 @@
+const dogYearOfBirth = 1991;
+const dogYearFuture = 2000;
+const dogYear = dogYearFuture - dogYearOfBirth;
