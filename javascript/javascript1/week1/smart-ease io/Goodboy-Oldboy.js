7e8a700b5b77ea2d14952a039e44c209ae5bedaf
Alaa Abdelbaki <a.eldeen91@gmail.com>
2022-10-18 12:04:16 +0200

    add Goodboy-oldboy file

--- javascript/javascript1/week1/smart-ease io/Goodboy-Oldboy.js
+++ javascript/javascript1/week1/smart-ease io/Goodboy-Oldboy.js
@@ -1,3 +1,10 @@
 const dogYearOfBirth = 1991;
 const dogYearFuture = 2000;
-const dogYear = dogYearFuture - dogYearOfBirth;
+const age = dogYearFuture - dogYearOfBirth;
+const shouldShowResultInDogYears = true;
+const dogYear = age + 10;
+if (shouldShowResultInDogYears) {
+    console.log(`Your dog will be ${dogYear} human years old in ${dogYearFuture}`)
+} else {
+    console.log(`Your dog will be ${age}  years old in ${dogYearFuture}`)
+}
