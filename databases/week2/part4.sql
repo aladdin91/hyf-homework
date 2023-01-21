32e888b8d78ef8f7ad7ea699c4c03278c09e655b
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-01-21 20:35:32 +0200

    modify part4

--- databases/week2/part4.sql
+++ databases/week2/part4.sql
@@ -36,39 +36,37 @@ CREATE TABLE
 create table
     book(
         id int(10) unsigned NOT NULL AUTO_INCREMENT,
-        category_name varchar(255) NOT NULL,
         publish_date date,
         price int NOT NULL,
         description varchar(255),
         isbn int NOT NULL,
         author_id int(10) unsigned NOT NULL,
         category_id int(10) unsigned NOT NULL,
-        rating_id int(10) unsigned NOT NULL,
+        rating_id int(10) unsigned,
         publisher_id int(10) unsigned NOT NULL,
         page_count int(10) unsigned NOT NULL,
         PRIMARY KEY (id),
         CONSTRAINT author FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE CASCADE,
         CONSTRAINT category FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE,
-        CONSTRAINT rating FOREIGN KEY (rating_id) REFERENCES rating(id) ON DELETE CASCADE,
-        CONSTRAINT publish FOREIGN KEY (publisher_id) REFERENCES publisher(id) ON DELETE CASCADE
+        CONSTRAINT rating FOREIGN KEY (rating_id) REFERENCES rating(id),
+        CONSTRAINT publish FOREIGN KEY (publisher_id) REFERENCES publisher(id)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 create table
     user(
         id int(10) unsigned NOT NULL AUTO_INCREMENT,
         username varchar(255) NOT NULL UNIQUE,
-        password int(10) NOT NULL,
+        password varchar(255) NOT NULL,
         email varchar(255) NOT NULL UNIQUE,
-        favourite_book_id int(10) unsigned NOT NULL,
-        favourite_author_id int(10) unsigned NOT NULL,
+        favourite_book_id int(10) unsigned,
+        favourite_author_id int(10) unsigned,
         PRIMARY KEY (id),
-        CONSTRAINT book FOREIGN KEY (favourite_book_id) REFERENCES book(id) ON DELETE CASCADE,
-        CONSTRAINT fav_author FOREIGN KEY (favourite_author_id) REFERENCES author(id) ON DELETE CASCADE
+        CONSTRAINT book FOREIGN KEY (favourite_book_id) REFERENCES book(id),
+        CONSTRAINT fav_author FOREIGN KEY (favourite_author_id) REFERENCES author(id)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 create table
     co_author(
-        -- id int(10) unsigned NOT NULL AUTO_INCREMENT,
         author_id int(10) unsigned NOT NULL,
         book_id int(10) unsigned NOT NULL,
         PRIMARY KEY (author_id, book_id),
