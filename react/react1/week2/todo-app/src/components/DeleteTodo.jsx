befe382f5dd5c3c3a49beb50840bc1e3b9c219e1
Alaa Abdelbaki <30771660+aladdin91@users.noreply.github.com>
2023-03-03 23:43:32 +0100

    Delete react/react1/week2/todo-app directory

--- react/react1/week2/todo-app/src/components/DeleteTodo.jsx
+++
@@ -1,9 +0,0 @@
-function DeleteTodo({ todos, setTodo, item }) {
-  const deleteTodo = () => {
-    setTodo(todos.filter((todo) => todo.id !== item.id));
-  };
-
-  return <button onClick={deleteTodo}>Delete</button>;
-}
-
-export default DeleteTodo;
